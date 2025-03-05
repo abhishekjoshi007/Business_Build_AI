dbeb5908ea08c1b7c19e2abf25ecf5b7fb56c3cb
koushik8686 <pinnukoushik1@gmail.com>
2025-03-05 22:51:24 +0530

    fixed image height

--- src/app/components/(marketing)/navbar.tsx
+++ src/app/components/(marketing)/navbar.tsx
@@ -25,89 +25,101 @@ export default function NavBar() {
       return
     }
     const resp = await signIn('google')
-    console.log(resp)
   }
   return (
     <Disclosure as="nav" className="bg-black">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-6xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <IconX className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <IconMenu className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                <img
+    {({ open }) => (
+      <>
+        <div className="mx-auto max-w-6xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            {/* Mobile menu button */}
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <span className="sr-only">Open main menu</span>
+                {open ? (
+                  <IconX className="block h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <IconMenu className="block h-6 w-6" aria-hidden="true" />
+                )}
+              </Disclosure.Button>
+            </div>
+
+            {/* Logo and navigation links */}
+            <div className="flex flex-1 items-center ">
+              {/* Logo */}
+              <div className="flex flex-shrink-0 items-center">
+                <Link href="/">
+                  <img
                     src="/logo.png"
-                    alt="Workflow"
-                    className="h-8 w-auto"
-                  />                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <Link
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          pathname == item.href
-                            ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium',
-                        )}
-                        aria-current={
-                          pathname == item.href ? 'page' : undefined
-                        }
-                      >
-                        {item.name}
-                      </Link>
-                    ))}
-                  </div>
+                    alt="Logo"
+                    className="h-12 w-auto sm:h-16"
+                  />
+                </Link>
+              </div>
+
+              {/* Desktop navigation links */}
+              <div className="hidden sm:block">
+                <div className="flex justify-start space-x-4">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        pathname === item.href
+                          ? 'bg-gray-900 text-white'
+                          : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'rounded-md px-3 py-2 text-sm font-medium'
+                      )}
+                      aria-current={pathname === item.href ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <Link
-                  href="/register"
-                  className='rounded-md px-3 py-1 font-medium border border-indigo-700 mr-3 hover:border-indigo-800 hover:text-gray-200'
-                >
-                  Register
-                </Link>
-                <button className='hover:text-gray-200' onClick={() => signIn()}>Sign In</button>
-              
-              </div>
+            </div>
+
+            {/* Register and Sign In buttons */}
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <Link
+                href="/register"
+                className="rounded-md px-3 py-1 font-medium border border-indigo-700 mr-3 hover:border-indigo-800 hover:text-gray-200 text-sm sm:text-base"
+              >
+                Register
+              </Link>
+              <button
+                className="hover:text-gray-200 text-sm sm:text-base"
+                onClick={() => signIn()}
+              >
+                Sign In
+              </button>
             </div>
           </div>
+        </div>
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium',
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+        {/* Mobile navigation links */}
+        <Disclosure.Panel className="sm:hidden">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            {navigation.map((item) => (
+              <Disclosure.Button
+                key={item.name}
+                as="a"
+                href={item.href}
+                className={classNames(
+                  pathname === item.href
+                    ? 'bg-gray-900 text-white'
+                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'block rounded-md px-3 py-2 text-base font-medium',
+                )}
+                aria-current={pathname === item.href ? 'page' : undefined}
+              >
+                {item.name}
+              </Disclosure.Button>
+            ))}
+          </div>
+        </Disclosure.Panel>
+      </>
+    )}
+  </Disclosure>
   )
 }
